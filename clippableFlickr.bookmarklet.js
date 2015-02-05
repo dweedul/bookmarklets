3f697647fbc2e91505e93f39d0c586dc4062c7c2
Dewayne Bagley <dewayne.bagley@gmail.com>
2015-02-05 02:34:38 +0000

    And so it begins...

---
+++ clippableFlickr.bookmarklet.js
@@ -0,0 +1,24 @@
+javascript: (function(){
+    
+    if (window.clippableFlickr) {
+        return;
+    }
+    window.clippableFlickr = { };
+    
+    // Add jQuery to the website
+    var script = document.createElement('script');
+    script.setAttribute('type', 'text/javascript');
+    script.setAttribute('charset', 'UTF-8');
+    script.setAttribute('src', 'http://example.com/script.js?r=' + Math.random());
+    document.documentElement.appendChild(script);
+    
+    script.onload = script.onreadstatechange = function() {
+        var rs = script.readyState;
+        if (!rs || rs === 'loaded' || rs === 'complete') {
+            script.onload = script.onreadstatechange = null;
+            
+            window.clippableFlickr.init();
+        }
+    }
+    
+}());
