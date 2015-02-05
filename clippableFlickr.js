16d928c31d3cfd356ae97f9bf97c93f7dcac6080
Dewayne Bagley <dewayne.bagley@gmail.com>
2015-02-05 04:06:31 +0000

    highlight zoomed images

--- clippableFlickr.js
+++ clippableFlickr.js
@@ -6,19 +6,21 @@ window.clippableFlickr = (function(window, document){
         
         title = document.title,
         host = document.location.host;
-    
+        
     var min_jQuery = "2.1.3";
     
-    if (window.jQuery === undefined || window.jQuery.fn.jquery < min_jQuery) {
-        var done = false;
-        var jQ = document.createElement('script');
-        jQ.src = 'https://ajax.googleapis.com/ajax/libs/jquery/' + min_jQuery + '/jquery.min.js'
-        jQ.onload = jQ.onreadstatechange = function(){
-            if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')){
-                done = true;
-            }
-        };
-        head.appendChild(jQ);
+    var _loadJQuery = function(){
+        if (window.jQuery === undefined || window.jQuery.fn.jquery < min_jQuery) {
+            var done = false;
+            var jQ = document.createElement('script');
+            jQ.src = 'https://ajax.googleapis.com/ajax/libs/jquery/' + min_jQuery + '/jquery.min.js'
+            jQ.onload = jQ.onreadstatechange = function(){
+                if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')){
+                    done = true;
+                }
+            };
+            head.appendChild(jQ);
+        }
     }
     
     var app = {};
