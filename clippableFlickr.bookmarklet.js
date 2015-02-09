a4f64d1652e53d6c64757cdbcf2f412c7794dba8
Dewayne Bagley <dweedul@users.noreply.github.com>
2015-02-08 18:05:04 -0600

    Update clippableFlickr.bookmarklet.js

--- clippableFlickr.bookmarklet.js
+++ clippableFlickr.bookmarklet.js
@@ -1,14 +1,32 @@
 javascript: (function() {
-    var min_jQuery = "2.1.3";
-
+    var jQueryUrl = 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js'
+    var scriptUrl = 'https://dweedul.github.io/bookmarklets/clippableFlickr.js?r=' + Math.random();
+    
+    function loadScript(url, success) {
+        var script=document.createElement('script');
+        script.setAttribute('type', 'text/javascript');
+        script.setAttribute('charset', 'UTF-8');
+        script.setAttribute('src', url);
+        document.documentElement.appendChild(script);
+        
+        script.onload = script.onreadystatechange = function() {
+            if (!script.readyState || script.readyState === 'loaded' || script.readyState === 'complete') {
+                script.onload = script.onreadystatechange = null;
+                success();
+            }
+        }
+    }
+    
     // Add jQuery
     if (!window.jQuery) {
+        loadScript(jQueryUrl, )
         var jQ = document.createElement('script');
         jQ.setAttribute('type', 'text/javascript');
         jQ.setAttribute('charset', 'UTF-8');
         jQ.src = 'https://ajax.googleapis.com/ajax/libs/jquery/' + min_jQuery + '/jquery.min.js';
         document.documentElement.appendChild(jQ);
-    }
+
+    };
 
 
     if (window.clippableFlickr) {
@@ -22,10 +40,10 @@ javascript: (function() {
     script.setAttribute('src', 'https://dweedul.github.io/bookmarklets/clippableFlickr.js?r=' + Math.random());
     document.documentElement.appendChild(script);
 
-    script.onload = script.onreadstatechange = function() {
+    script.onload = script.onreadystatechange = function() {
         var rs = script.readyState;
         if (!rs || rs === 'loaded' || rs === 'complete') {
-            script.onload = script.onreadstatechange = null;
+            script.onload = script.onreadystatechange = null;
 
             window.clippableFlickr.init();
         }
